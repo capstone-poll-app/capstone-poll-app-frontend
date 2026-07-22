9f94d197caec1fa7a6af02c9c8800961dbb22463
jordanrxavier <jordanrxavier@gmail.com>
2026-07-22 17:10:53 -0400

    ResultsPage

--- polling-app/src/pages/CreatePoll.jsx
+++ polling-app/src/pages/CreatePoll.jsx
@@ -43,7 +43,7 @@ function CreatePoll() {
         setFormError("")
 
         try {
-            const response = await fetch("http://localhost:3000/polls", {
+            const response = await fetch("/polls", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ title, description, options: nonEmptyOptions }),
