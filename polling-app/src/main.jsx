a6a0a56bea9778bb2b146f8571d660ccb12b15e8
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-07-23 07:59:35 -0400

    commit: moved to scss for styling

--- polling-app/src/main.jsx
+++ polling-app/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router'
 import AppRoutes from './routes.jsx'
-import './index.css'
+import './styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
